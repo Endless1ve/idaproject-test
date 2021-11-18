1723b85a9e8febb0c28918998a7e6ae70c054de3
Artem Tulupov <stalin.dignitas70@gmail.com>
2021-11-18 19:16:38 +0300

    upload files

---
+++ src/components/AppList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="body"></div>
+</template>
+
+<script>
+export default {
+  name: 'AppList',
+  props: {
+  }
+}
+</script>
+
+<style lang="scss">
+$color: black;
+  .body{
+    width: 200px;
+    height: 200px;
+    background-color: $color;
+  }
+</style>
